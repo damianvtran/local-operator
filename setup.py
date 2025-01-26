0686c3b48b81da21a90490663f0da2c3a75080ad
Damian Tran <damianvtran@gmail.com>
2025-01-25 22:06:46 -0500

    Update package manifests, restructure files with main entrypoint in CLI

--- setup.py
+++ setup.py
@@ -2,21 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="local-operator",
-    version="0.0.1",
     packages=find_packages(),
+    py_modules=["local_operator.cli"],
     entry_points={
         "console_scripts": [
-            "local-operator = local_operator.main:main",
+            "local-operator = local_operator.cli:main",
         ],
     },
     install_requires=[
         "langchain-openai>=0.3.2",
         "python-dotenv>=1.0.1",
         "pydantic>=2.10.6",
-        "flake8>=7.1.0",
-        "black>=24.4.2",
-        "isort>=5.13.2",
-        "pyright>=1.1.368",
     ],
     python_requires=">=3.12",
     extras_require={
