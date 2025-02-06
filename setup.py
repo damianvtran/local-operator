6d1feb77f2c51eff6bed6938ecdea827959b6cd5
Damian Tran <damianvtran@gmail.com>
2025-02-06 11:08:45 -0500

    Update configuration and dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         "langchain-openai>=0.3.2",
         "langchain-ollama>=0.2.2",
         "langchain-anthropic>=0.3.3",
+        "langchain-google-genai>=2.0.8",
         "langchain>=0.3.14",
         "python-dotenv>=1.0.1",
         "pydantic>=2.10.6",
